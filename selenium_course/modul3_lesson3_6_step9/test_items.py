ecd5e40e66337dbf4694982eafd4fb0979aba297
Семён Ивакаев <s.ivakaev@ctm.ru>
2019-12-03 12:19:08 +0300

    Update files

--- selenium_course/modul3_lesson3_6_step9/test_items.py
+++ selenium_course/modul3_lesson3_6_step9/test_items.py
@@ -1,11 +1,14 @@
 import time
+from selenium import webdriver
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    y = browser.find_element_by_css_selector("#add_to_basket_form button")
+    time.sleep(30)
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
     time.sleep(3)
-    assert y == browser.find_element_by_css_selector("#add_to_basket_form button"), \
-           f"Button is false"
+    assert button, "Button not on this page"
+
     
